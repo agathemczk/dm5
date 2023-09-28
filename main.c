aae6f701b23af1efb8b659180464013798d236c0
agathemczk <agathe.maciaszczyk@icloud.com>
2023-09-28 19:38:01 +0200

    commit

--- main.c
+++ main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+void convertirBase(int number, int targetedbase) {
+    int value = number;
+    char result[32];
+    int index = 0;
 
+    while (number > 0) {
+        int rest = number % targetedbase;
+        if (rest < 10) {
+            result[index] = rest + '0';
+        } else {
+            result[index] = rest - 10 + 'A';
+        }
+        number /= targetedbase;
+        index++;
+    }
+
+    printf("Le nombre %d en base %d est équivalent à ", value, targetedbase);
+
+    for (int i = index - 1; i >= 0; i--) {
+        printf("%c", result[i]);
+    }
+
+    printf("\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+    int nombre,baseCible;
+    printf("Entrez un nombre : \n");
+    scanf("%d", &nombre);
+    getchar();
+    printf("Entrez la base cible (2 pour binaire,16 pour hexadécimal) : \n");
+    scanf("%d", &baseCible);
+    convertirBase(nombre,baseCible);
     return 0;
 }
